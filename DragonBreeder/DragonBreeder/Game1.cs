cec71483d5a19bae86d3a2c843a91192e5c3b34c
Adam <tasaq313@gmail.com>
2013-03-20 23:12:32 +0100

    Model loading fixed

--- DragonBreeder/DragonBreeder/Game1.cs
+++ DragonBreeder/DragonBreeder/Game1.cs
@@ -3,25 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using SharpDX;
-using SharpDX.Toolkit;
 using SharpDX.Toolkit.Graphics;
+
+using SharpDX;
+using SharpDX.Direct3D;
+using SharpDX.Direct3D11;
+using SharpDX.Toolkit;
+using SharpDX.Toolkit.Input;
+using Matrix = SharpDX.Matrix;
+using Color = SharpDX.Color;
 namespace DragonBreeder
 {
     class Game1 : Game
     {
             private GraphicsDeviceManager graphicsDeviceManager;
-
+            Model model;
             /// <summary>
-            /// Initializes a new instance of the <see cref="HelloWorldGame" /> class.
+            /// Initializes a new instance of the <see cref="Game1" /> class.
             /// </summary>
             public Game1()
             {
-                // Creates a graphics manager. This is mandatory.
                 graphicsDeviceManager = new GraphicsDeviceManager(this);
-                // Setup the relative directory to the executable directory
-                // for loading contents with the ContentManager
                 Content.RootDirectory = "Content";
+            }
+            protected override void LoadContent()
+            {
+                model = Content.Load<Model>("sphere");
+                base.LoadContent();
             }
 
             protected override void Initialize()
@@ -32,8 +40,9 @@ namespace DragonBreeder
 
             protected override void Draw(GameTime gameTime)
             {
-                // Clears the screen with the Color.CornflowerBlue
+
                 GraphicsDevice.Clear(Color.CornflowerBlue);
+                model.Draw(GraphicsDevice, Matrix.Translation(0, 0, 0), Matrix.LookAtLH(new Vector3(0, 0, -10), new Vector3(0, 0, 0), new Vector3(0, 1, 0)), Matrix.PerspectiveFovLH(MathUtil.DegreesToRadians(45), 16.0f / 9.0f, 0.1f, 100.0f));
                 base.Draw(gameTime);
             }
         }
