2854d4ee2886b267243c98ebc829c608589ce1c6
Adam <tasaq313@gmail.com>
2013-03-31 14:33:17 +0200

    Clean project in JBBRXG11

---
+++ DragonBreeder/DragonBreeder/DragonBreeder/Game1.cs
@@ -0,0 +1,115 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using JBBRXG11;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Audio;
+using GameTime = Microsoft.Xna.Framework.GameTime;
+using Vector2 = Microsoft.Xna.Framework.Vector2;
+using Vector3 = Microsoft.Xna.Framework.Vector3;
+using Vector4 = Microsoft.Xna.Framework.Vector4;
+using Matrix = Microsoft.Xna.Framework.Matrix;
+using Color = Microsoft.Xna.Framework.Color;
+using Rectangle = Microsoft.Xna.Framework.Rectangle;
+using SpriteSortMode = Microsoft.Xna.Framework.Graphics.SpriteSortMode;
+using SpriteEffects = Microsoft.Xna.Framework.Graphics.SpriteEffects;
+using BlendFunction = Microsoft.Xna.Framework.Graphics.BlendFunction;
+using Blend = Microsoft.Xna.Framework.Graphics.Blend;
+using ColorWriteChannels = Microsoft.Xna.Framework.Graphics.ColorWriteChannels;
+using StencilOperation = Microsoft.Xna.Framework.Graphics.StencilOperation;
+using CompareFunction = Microsoft.Xna.Framework.Graphics.CompareFunction;
+using FillMode = Microsoft.Xna.Framework.Graphics.FillMode;
+using CullMode = Microsoft.Xna.Framework.Graphics.CullMode;
+using IndexElementSize = Microsoft.Xna.Framework.Graphics.IndexElementSize;
+using DepthFormat = Microsoft.Xna.Framework.Graphics.DepthFormat;
+using SurfaceFormat = Microsoft.Xna.Framework.Graphics.SurfaceFormat;
+using VertexPositionColor = Microsoft.Xna.Framework.Graphics.VertexPositionColor;
+using VertexPositionColorTexture = Microsoft.Xna.Framework.Graphics.VertexPositionColorTexture;
+using VertexPositionNormalTexture = Microsoft.Xna.Framework.Graphics.VertexPositionNormalTexture;
+using VertexPositionTexture = Microsoft.Xna.Framework.Graphics.VertexPositionTexture;
+using VertexElementFormat = Microsoft.Xna.Framework.Graphics.VertexElementFormat;
+using VertexElementUsage = Microsoft.Xna.Framework.Graphics.VertexElementUsage;
+using VertexElement = Microsoft.Xna.Framework.Graphics.VertexElement;
+using ClearOptions = Microsoft.Xna.Framework.Graphics.ClearOptions;
+using MathHelper = Microsoft.Xna.Framework.MathHelper;
+
+namespace DragonBreeder
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : JBBRXG11.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            Window.Title = "DragonBreeder";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // Allows the game to exit
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape)) Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(new Color(162, 173, 208, 255)/*Wild blue yonder*/);
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
